c0494498e0eea0d01b7a407231e4cb7aee899b78
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-02-08 19:10:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", " {filename}\n\n<b>YT Channel</b> :- https://youtube.com/@HK_Production1?si=4SGtv4GocenmKoUW")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -56,7 +56,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "Hey Bro/Sis Don't send me messages directly.... I'm only File Share bot!"
+USER_REPLY_TEXT = "Hey {first}\nDon't send me messages directly.... I'm private file share bot❗"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
