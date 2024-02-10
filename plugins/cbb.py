c45f33566b03da10f7698edb11edc2a8a632963d
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-02-10 16:59:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>𝐑𝐀𝐕𝐀𝐀𝐍𝐀</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://t.me/TG_LINKS_CHANNEL2'>Library</a>\n○ Request Group : <a href='https://t.me/movies_hub_official2'>Click here</a>\n○ Channel : @TG_LINKS_CHANNEL2</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>𝐑𝐀𝐕𝐀𝐀𝐍𝐀</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://t.me/TG_LINKS_CHANNEL1'>Library</a>\n○ Request Group : <a href='https://t.me/+RfQK2YwfLVFmNzdl'>Click here</a>\n○ Channel : @TG_LINKS_CHANNEL1</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
